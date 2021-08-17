a3c9b6dd3d11793140aa330251db94bf72481441
Soner Eker <sonereker@gmail.com>
2021-08-17 23:39:53 +0300

    Better way
    Signed-off-by: Soner Eker <sonereker@gmail.com>

--- t4_1_frog_river.go
+++ t4_1_frog_river.go
@@ -1,21 +1,13 @@
 package main
 
 func FrogRiver(X int, A []int) int {
-	var requiredSteps []int
-	for i := 1; i < X+1; i++ {
-		requiredSteps = append(requiredSteps, i)
-	}
-
+	var requiredSteps = make(map[int]int, X)
 	for i, val := range A {
 		if val <= X {
-			for k, step := range requiredSteps {
-				if step == val {
-					requiredSteps = append(requiredSteps[:k], requiredSteps[k+1:]...)
-				}
-			}
+			requiredSteps[val] = 1
 		}
 
-		if len(requiredSteps) == 0 {
+		if len(requiredSteps) == X {
 			return i
 		}
 	}
