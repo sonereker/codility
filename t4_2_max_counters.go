5b82adb87a53bc50f3dd2d69268aa672d55a789e
Soner Eker <sonereker@gmail.com>
2021-08-18 18:00:15 +0300

    Fix T4-4
    Signed-off-by: Soner Eker <sonereker@gmail.com>

--- t4_2_max_counters.go
+++ t4_2_max_counters.go
@@ -1,24 +1,35 @@
 package main
 
+//MaxCounters O(N + M)
 func MaxCounters(N int, A []int) []int {
 	counter := make([]int, N)
-	var max int
+	var currentMax int
+	var resetMax int
 
 	for _, val := range A {
 		if 1 <= val && val <= N {
+			if resetMax != 0 && counter[val-1] < resetMax {
+				counter[val-1] = resetMax
+			}
+
 			counter[val-1]++
-			if counter[val-1] > max {
-				max = counter[val-1]
+			if counter[val-1] > currentMax {
+				currentMax = counter[val-1]
 			}
 		}
 
 		if val == N+1 {
-			for k, _ := range counter {
-				if counter[k] < max {
-					counter[k] = max
-				}
+			resetMax = currentMax
+		}
+	}
+
+	if resetMax != 0 {
+		for i, cval := range counter {
+			if cval < resetMax {
+				counter[i] = resetMax
 			}
 		}
 	}
+
 	return counter
 }
