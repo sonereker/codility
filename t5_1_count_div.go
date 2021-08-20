6b133456e5fc6a5cb0a41def39df68091af5260d
Soner Eker <sonereker@gmail.com>
2021-08-20 18:40:31 +0300

    Another try T5-1
    Signed-off-by: Soner Eker <sonereker@gmail.com>

--- t5_1_count_div.go
+++ t5_1_count_div.go
@@ -3,7 +3,21 @@ package main
 import "math"
 
 func CountDiv(A int, B int, K int) int {
-	return int(Round(float64((B + 1 - A) / K)))
+	if A == B {
+		if B%K == 0 {
+			return 1
+		} else {
+			return 0
+		}
+	}
+
+	res := int(Round(float64(B-A) / float64(K)))
+
+	if A == 0 {
+		res++
+	}
+
+	return res
 }
 
 func Round(x float64) float64 {
--- t5_1_count_div_test.go
+++ t5_1_count_div_test.go
@@ -9,18 +9,20 @@ func TestCountDiv(t *testing.T) {
 		K   int
 		res int
 	}{
+		{A: 0, B: 13, K: 2, res: 7},
+		{A: 0, B: 14, K: 2, res: 8},
 		{A: 11, B: 345, K: 17, res: 20},
 		{A: 10, B: 10, K: 5, res: 1},
-		{A: 0, B: 22, K: 3, res: 7},
+		{A: 0, B: 22, K: 3, res: 8},
 		{A: 6, B: 11, K: 2, res: 3},
 		{A: 1, B: 2, K: 2, res: 1},
-		{A: 1, B: 2000_000_000, K: 2, res: 1000000000},
+		{A: 1, B: 2_000_000_000, K: 2, res: 1_000_000_000},
 	}
 
 	for _, test := range tests {
 		result := CountDiv(test.A, test.B, test.K)
 		if result != test.res {
-			t.Errorf("Expected %d, got %d", test.res, result)
+			t.Errorf("%v..%v - Expected %d, got %d", test.A, test.B, test.res, result)
 		}
 	}
 }
