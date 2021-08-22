b0ef514f1faa8fb5bc1fbe5f8c739781fa605c50
Soner Eker <sonereker@gmail.com>
2021-08-22 11:30:03 +0300

    T5-2 Prefix sums
    Signed-off-by: Soner Eker <sonereker@gmail.com>

--- t5_2_genomic_range_query.go
+++ t5_2_genomic_range_query.go
@@ -1,29 +1,45 @@
 package main
 
-import "sort"
-
 func GenomicRangeQuery(S string, P []int, Q []int) []int {
-	var result []int
-
-	dna := make(map[string]int)
-	dna["A"] = 1
-	dna["C"] = 2
-	dna["G"] = 3
-	dna["T"] = 4
-
-	var stringAsArray []int
-	for i := 0; i < len(S); i++ {
-		stringAsArray = append(stringAsArray, dna[string(S[i])])
+	var sequenceLength = len(S) + 1
+	var genoms = make([][]int, 3)
+	for i := range genoms {
+		genoms[i] = make([]int, sequenceLength)
 	}
 
-	for i := 0; i < len(P); i++ {
-		start := P[i]
-		end := Q[i]
+	var a, c, g int
+	for i := 0; i < len(S); i++ {
+		if string(S[i]) == "A" {
+			a = 1
+		}
 
-		impactFactors := make([]int, len(stringAsArray[start:end+1]))
-		copy(impactFactors, stringAsArray[start:end+1])
-		sort.Ints(impactFactors)
-		result = append(result, impactFactors[0])
+		if string(S[i]) == "C" {
+			c = 1
+		}
+
+		if string(S[i]) == "G" {
+			g = 1
+		}
+
+		genoms[0][i+1] = genoms[0][i] + a
+		genoms[1][i+1] = genoms[1][i] + c
+		genoms[2][i+1] = genoms[2][i] + g
+	}
+
+	var result = make([]int, len(P))
+	for i := 0; i < len(P); i++ {
+		var fromIndex = P[i]
+		var toIndex = Q[i] + 1
+
+		if genoms[0][toIndex]-genoms[0][fromIndex] > 0 {
+			result[i] = 1
+		} else if genoms[1][toIndex]-genoms[1][fromIndex] > 0 {
+			result[i] = 2
+		} else if genoms[2][toIndex]-genoms[2][fromIndex] > 0 {
+			result[i] = 3
+		} else {
+			result[i] = 4
+		}
 	}
 
 	return result
