0b5c7cee9b5791e5ca6e104a5a3aaeb6c48c1dbe
Soner Eker <sonereker@gmail.com>
2021-09-08 00:29:28 +0300

    Apply prefix sums
    Signed-off-by: Soner Eker <sonereker@gmail.com>

--- t5_2_genomic_range_query.go
+++ t5_2_genomic_range_query.go
@@ -1,46 +1,56 @@
+// See https://codility.com/media/train/3-PrefixSums.pdf
+
 package main
 
+import "fmt"
+
 func GenomicRangeQuery(S string, P []int, Q []int) []int {
-	var sequenceLength = len(S) + 1
-	var genoms = make([][]int, 3)
-	for i := range genoms {
-		genoms[i] = make([]int, sequenceLength)
+	sLen := len(S)
+	pLen := len(P)
+	ps := make([][]int, 4)
+	for i := 0; i < 4; i++ {
+		ps[i] = make([]int, sLen+1)
 	}
 
-	var a, c, g int
-	for i := 0; i < len(S); i++ {
-		if string(S[i]) == "A" {
-			a = 1
+	for i := 0; i < sLen; i++ {
+		c := S[i : i+1]
+		if c == "A" {
+			ps[0][i+1] = 1
 		}
-
-		if string(S[i]) == "C" {
-			c = 1
+		if c == "C" {
+			ps[1][i+1] = 1
 		}
-
-		if string(S[i]) == "G" {
-			g = 1
+		if c == "G" {
+			ps[2][i+1] = 1
 		}
-
-		genoms[0][i+1] = genoms[0][i] + a
-		genoms[1][i+1] = genoms[1][i] + c
-		genoms[2][i+1] = genoms[2][i] + g
+		if c == "T" {
+			ps[3][i+1] = 1
+		}
 	}
 
-	var result = make([]int, len(P))
-	for i := 0; i < len(P); i++ {
-		var fromIndex = P[i]
-		var toIndex = Q[i] + 1
+	// Calculate prefix sums
+	fmt.Println("-")
+	for i := 0; i < 4; i++ {
+		for j := 1; j < sLen+1; j++ {
+			ps[i][j] += ps[i][j-1]
+		}
+		fmt.Println(ps[i])
+	}
 
-		if genoms[0][toIndex]-genoms[0][fromIndex] > 0 {
+	result := make([]int, pLen)
+	for i := 0; i < pLen; i++ {
+		fromIndex := P[i]
+		toIndex := Q[i] + 1
+
+		if ps[0][toIndex]-ps[0][fromIndex] > 0 {
 			result[i] = 1
-		} else if genoms[1][toIndex]-genoms[1][fromIndex] > 0 {
+		} else if ps[1][toIndex]-ps[1][fromIndex] > 0 {
 			result[i] = 2
-		} else if genoms[2][toIndex]-genoms[2][fromIndex] > 0 {
+		} else if ps[2][toIndex]-ps[2][fromIndex] > 0 {
 			result[i] = 3
 		} else {
 			result[i] = 4
 		}
 	}
-
 	return result
 }
--- t5_2_genomic_range_query_test.go
+++ t5_2_genomic_range_query_test.go
@@ -13,7 +13,8 @@ func TestGenomicRangeQuery(t *testing.T) {
 		res []int
 	}{
 		{S: "CAGCCTA", P: []int{2, 5, 0}, Q: []int{4, 5, 6}, res: []int{2, 4, 1}},
-		{S: "TC", P: []int{0, 0, 1}, Q: []int{0, 1, 1}, res: []int{4, 2, 2}},
+		{S: "A", P: []int{0}, Q: []int{0}, res: []int{1}},
+		{S: "AC", P: []int{0, 0, 1}, Q: []int{0, 1, 1}, res: []int{1, 1, 2}},
 	}
 
 	for _, test := range tests {
